62c138a8a08bff0ecfd9a19857994abb67384a2d
Alex <alexqw@007gmail.com>
2023-10-04 20:42:39 +0200

    init discovery

--- resourceservice/src/main/java/com/alex/rest/ResourceController.java
+++ resourceservice/src/main/java/com/alex/rest/ResourceController.java
@@ -12,7 +12,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
--- songservice/src/main/java/org/alex/SongserviceApplication.java
+++ songservice/src/main/java/org/alex/SongserviceApplication.java
@@ -2,8 +2,10 @@ package org.alex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SongserviceApplication {
 
 	public static void main(String[] args) {
